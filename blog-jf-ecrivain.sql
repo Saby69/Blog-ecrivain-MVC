b598e757ec50e1df0df752bd76f48114a1751c46
Unknown <scarruezco@gmail.com>
2018-02-15 10:59:48 +0100

    maj bdd

--- blog-jf-ecrivain.sql
+++ blog-jf-ecrivain.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Mer 14 Février 2018 à 14:42
+-- Généré le :  Jeu 15 Février 2018 à 09:58
 -- Version du serveur :  5.7.14
 -- Version de PHP :  5.6.25
 
@@ -40,16 +40,16 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`id`, `post_id`, `alert`, `author`, `comment`, `comment_date`) VALUES
-(58, 23, 1, 'Astérix Épéril', 'Oh la la !!! ', '2018-02-14 11:02:41'),
+(58, 23, 2, 'Astérix Épéril', 'Oh la la !!! ', '2018-02-14 11:02:41'),
 (10, 1, 1, 'Saby', 'test', '2018-01-19 17:49:53'),
-(59, 24, 1, 'Daisy Des Rata', 'J\'aime beaucoup le lorem ipsum !!!', '2018-02-14 11:03:47'),
+(59, 24, 3, 'Daisy Des Rata', 'J\'aime beaucoup le lorem ipsum !!!', '2018-02-14 11:03:47'),
 (60, 24, 1, 'Ella Lapêche', 'C\'est nul !!!', '2018-02-14 11:04:38'),
 (61, 24, 1, 'Eva Porée', 'Mais ou suis je ?', '2018-02-14 11:05:01'),
-(62, 26, 2, 'Guy Tar', 'J\'aime ce coté chantant de l\'écriture !!!', '2018-02-14 11:05:42'),
+(62, 26, 1, 'Guy Tar', 'J\'aime ce coté chantant de l\'écriture !!!', '2018-02-14 11:05:42'),
 (52, 16, 1, 'QQQ', 'qqqq', '2018-02-07 17:56:16'),
 (48, 1, 1, 'QQQQ', 'qqq', '2018-01-31 19:03:26'),
 (56, 23, 1, 'Jean Aymar', 'Bravo j\'adore votre style !!', '2018-02-14 11:00:54'),
-(57, 23, 3, 'Abel Auboisdormant', 'C\'est un peu soporifique ce 1er chapitre !!', '2018-02-14 11:02:02'),
+(63, 23, 1, 'Saby', 'Test', '2018-02-15 08:08:17'),
 (54, 21, 1, 'Saby', 'Test bouton', '2018-02-08 07:05:29'),
 (55, 21, 1, 'Saby', 'Test mentorat', '2018-02-08 08:22:28');
 
@@ -76,7 +76,7 @@ INSERT INTO `posts` (`id`, `title`, `content`, `author`, `datecreation`) VALUES
 (23, 'Chapitre 1', '<p>Accenderat super his incitatum propositum ad nocendum aliqua mulier vilis, quae ad palatium ut poposcerat intromissa insidias ei latenter obtendi prodiderat a militibus obscurissimis. quam Constantina exultans ut in tuto iam locata mariti salute muneratam vehiculoque inpositam per regiae ianuas emisit in publicum, ut his inlecebris alios quoque ad indicanda proliceret paria vel maiora.</p>\r\n<p>Post quorum necem nihilo lenius ferociens Gallus ut leo cadaveribus pastus multa huius modi scrutabatur. quae singula narrare non refert, me professione modum, quod evitandum est, excedamus.</p>\r\n<p>Sed fruatur sane hoc solacio atque hanc insignem ignominiam, quoniam uni praeter se inusta sit, putet esse leviorem, dum modo, cuius exemplo se consolatur, eius exitum expectet, praesertim cum in Albucio nec Pisonis libidines nec audacia Gabini fuerit ac tamen hac una plaga conciderit, ignominia senatus.</p>', 'Jean Forteroche', '2018-02-14 10:54:00'),
 (24, 'Chapitre 2', '<p>Post quorum necem nihilo lenius ferociens Gallus ut leo cadaveribus pastus multa huius modi scrutabatur. quae singula narrare non refert, me professione modum, quod evitandum est, excedamus.</p>\r\n<p>Sed fruatur sane hoc solacio atque hanc insignem ignominiam, quoniam uni praeter se inusta sit, putet esse leviorem, dum modo, cuius exemplo se consolatur, eius exitum expectet, praesertim cum in Albucio nec Pisonis libidines nec audacia Gabini fuerit ac tamen hac una plaga conciderit, ignominia senatus.</p>\r\n<p>Superatis Tauri montis verticibus qui ad solis ortum sublimius attolluntur, Cilicia spatiis porrigitur late distentis dives bonis omnibus terra, eiusque lateri dextro adnexa Isauria, pari sorte uberi palmite viget et frugibus minutis, quam mediam navigabile flumen Calycadnus interscindit.</p>', 'Jean Forteroche', '2018-02-14 10:54:52'),
 (25, 'Chapitre 3', '<p>Sed fruatur sane hoc solacio atque hanc insignem ignominiam, quoniam uni praeter se inusta sit, putet esse leviorem, dum modo, cuius exemplo se consolatur, eius exitum expectet, praesertim cum in Albucio nec Pisonis libidines nec audacia Gabini fuerit ac tamen hac una plaga conciderit, ignominia senatus.</p>\r\n<p>Superatis Tauri montis verticibus qui ad solis ortum sublimius attolluntur, Cilicia spatiis porrigitur late distentis dives bonis omnibus terra, eiusque lateri dextro adnexa Isauria, pari sorte uberi palmite viget et frugibus minutis, quam mediam navigabile flumen Calycadnus interscindit.</p>\r\n<p>Tempore quo primis auspiciis in mundanum fulgorem surgeret victura dum erunt homines Roma, ut augeretur sublimibus incrementis, foedere pacis aeternae Virtus convenit atque Fortuna plerumque dissidentes, quarum si altera defuisset, ad perfectam non venerat summitatem.</p>', 'Jean Forteroche', '2018-02-14 10:56:04'),
-(26, 'Chapitre 4', '<p>Iamque lituis cladium concrepantibus internarum non celate ut antea turbidum saeviebat ingenium a veri consideratione detortum et nullo inpositorum vel conpositorum fidem sollemniter inquirente nec discernente a societate noxiorum insontes velut exturbatum e iudiciis fas omne discessit, et causarum legitima silente defensione carnifex rapinarum sequester et obductio capitum et bonorum ubique multatio versabatur per orientales provincias, quas recensere puto nunc oportunum absque Mesopotamia digesta, cum bella Parthica dicerentur, et Aegypto, quam necessario aliud reieci ad tempus.</p>\r\n<p>Alii nullo quaerente vultus severitate adsimulata patrimonia sua in inmensum extollunt, cultorum ut puta feracium multiplicantes annuos fructus, quae a primo ad ultimum solem se abunde iactitant possidere, ignorantes profecto maiores suos, per quos ita magnitudo Romana porrigitur, non divitiis eluxisse sed per bella saevissima, nec opibus nec victu nec indumentorum vilitate gregariis militibus discrepantes opposita cuncta superasse virtute.</p>\r\n<p>Quod cum ita sit, paucae domus studiorum seriis cultibus antea celebratae nunc ludibriis ignaviae torpentis exundant, vocali sonu, perflabili tinnitu fidium resultantes. denique pro philosopho cantor et in locum oratoris doctor artium ludicrarum accitur et bybliothecis sepulcrorum ritu in perpetuum clausis organa fabricantur hydraulica, et lyrae ad speciem carpentorum ingentes tibiaeque et histrionici gestus instrumenta non levia.</p>', 'Jean Forteroche', '2018-02-14 10:58:22');
+(29, 'Chapitre 4', '<p>Quod cum ita sit, paucae domus studiorum seriis cultibus antea celebratae nunc ludibriis ignaviae torpentis exundant, vocali sonu, perflabili tinnitu fidium resultantes. denique pro philosopho cantor et in locum oratoris doctor artium ludicrarum accitur et bybliothecis sepulcrorum ritu in perpetuum clausis organa fabricantur hydraulica, et lyrae ad speciem carpentorum ingentes tibiaeque et histrionici gestus instrumenta non levia.</p>\r\n<p>&nbsp;</p>\r\n<p>Haec ubi latius fama vulgasset missaeque relationes adsiduae Gallum Caesarem permovissent, quoniam magister equitum longius ea tempestate distinebatur, iussus comes orientis Nebridius contractis undique militaribus copiis ad eximendam periculo civitatem amplam et oportunam studio properabat ingenti, quo cognito abscessere latrones nulla re amplius memorabili gesta, dispersique ut solent avia montium petiere celsorum.</p>\r\n<p>&nbsp;</p>\r\n<p>Victus universis caro ferina est lactisque abundans copia qua sustentantur, et herbae multiplices et siquae alites capi per aucupium possint, et plerosque mos vidimus frumenti usum et vini penitus ignorantes.</p>', 'Jean Forteroche', '2018-02-14 17:02:26');
 
 -- --------------------------------------------------------
 
@@ -95,7 +95,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `user`, `password`) VALUES
-(1, 'Saby', 'admin');
+(1, 'Saby', '$2y$10$hXPw1qgFKrhR0r0XLeZZuO.EfCWHh2V8eyrMBk9QtUSUbAwLZfwSG');
 
 --
 -- Index pour les tables exportées
@@ -127,12 +127,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 --
 -- AUTO_INCREMENT pour la table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 --
 -- AUTO_INCREMENT pour la table `users`
 --
